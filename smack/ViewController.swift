f7f863761e6927a4f4c40c584473014e15a8018b
ziad <ziadelnady555@gmail.com>
2020-06-15 07:46:52 -0700

    intial commit

---
+++ smack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  smack
+//
+//  Created by Ziad Ahmed Said Ahmed on 6/15/20.
+//  Copyright © 2020 Ziad Ahmed Said Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
