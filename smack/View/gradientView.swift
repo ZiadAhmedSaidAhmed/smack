ba1e00794f7fbd0fe191859997810d891002c86c
ziad <ziadelnady555@gmail.com>
2020-06-17 03:24:59 -0700

    channel vc first version done

---
+++ smack/View/gradientView.swift
@@ -0,0 +1,34 @@
+//
+//  gradientView.swift
+//  smack
+//
+//  Created by Ziad Ahmed Said Ahmed on 6/16/20.
+//  Copyright © 2020 Ziad Ahmed Said Ahmed. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class gradientView: UIView {
+
+    @IBInspectable var topColor: UIColor = #colorLiteral(red: 0.1725490196, green: 0.831372549, blue: 0.8470588235, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var buttomColor: UIColor = #colorLiteral(red: 0.2901960784, green: 0.3019607843, blue: 0.8470588235, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    override func layoutSubviews() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [topColor.cgColor, buttomColor.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at: 0)
+    }
+}
