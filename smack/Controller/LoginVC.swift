a2cdcabea2032b4116a3b962ff12a1875ee09c10
ziad <ziadelnady555@gmail.com>
2020-06-17 05:23:44 -0700

    loginVC Design Done

---
+++ smack/Controller/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVC.swift
+//  smack
+//
+//  Created by Ziad Ahmed Said Ahmed on 6/17/20.
+//  Copyright © 2020 Ziad Ahmed Said Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func exitButtonPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ smack/Utilities/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  smack
+//
+//  Created by Ziad Ahmed Said Ahmed on 6/17/20.
+//  Copyright © 2020 Ziad Ahmed Said Ahmed. All rights reserved.
+//
+
+import Foundation
+
+let TO_LOGIN = "toLoginVC"
